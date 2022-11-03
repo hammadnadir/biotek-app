abf8d473c5469cbd74045bac1658df8c2f701687
Muhammad Waseem <biztekapps.dev@gmail.com>
2022-11-03 21:48:41 +0500

    first commit dd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import logo from './logo.svg';
 import './App.scss';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import { Login, Signup } from './components/auth';
+import { getCurrentUser } from './utils';
 
 function App() {
+
+  const RequireAuth = ({ children }) => {
+    let auth = getCurrentUser();
+    let location = useLocation();
+    
+    if (!auth) {
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+    return children;
+  }; 
+
   return (
     <Routes>
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            {/* <Layout /> */}
+            <Signup />
+          </RequireAuth>
+        }
+      ></Route>
       <Route path='/' element={<Login />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
+      {/* <Route path='/signup' element={<Signup />} /> */}
     </Routes>
   );
 }
