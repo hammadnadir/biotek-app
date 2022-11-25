03df255b4eb3e4cc6e956b7bd7b841ee49b5cc11
Muhammad Waseem <biztekapps.dev@gmail.com>
2022-11-25 18:55:44 +0500

    okkkkkkkkkkkkkkkkkkkkkkkkkkk

--- src/components/Inventory/InventoryData/index.jsx
+++ src/components/Inventory/InventoryData/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./styles.scss";
 
 function InventoryData() {
@@ -12,14 +12,30 @@ function InventoryData() {
             <Button>Stock Ledger</Button>
           </Container>
         </div>
-        <div className="bottom_data">
+        <div className="main_inventory">
+          <div className="bottom_data">
+            <Container>
+              <h4>Pattern</h4>
+              <div className="three_btns">
+                <Button>Board</Button>
+                <Button>PATTERN</Button>
+                <Button>Sheet</Button>
+              </div>
+            </Container>
+          </div>
           <Container>
-            <h4>Pattern</h4>
-            <div className="three_btns">
-               <Button>Board</Button>
-               <Button>PATTERN</Button>
-               <Button>Sheet</Button>
-            </div>
+           <Row className="main_head">
+               <Col xs={1} className="head"><p>ID</p></Col>
+               <Col xs={3} className="head"><p>Name</p></Col>
+               <Col xs={3} className="head"><p>Product Type</p></Col>
+               <Col xs={5} className="head"><p>Stock In Hand<span>(Sheet)</span></p></Col>
+           </Row>
+           <Row>
+           <Col xs={1} className="head"><p>ID</p></Col>
+               <Col xs={3} className="head"><p>Name</p></Col>
+               <Col xs={3} className="head"><p>Product Type</p></Col>
+               <Col xs={5} className="head"><p>Stock In Hand<span>(Sheet)</span></p></Col>
+           </Row>
           </Container>
         </div>
       </div>
---
+++ src/components/Vouchers/DeleteModal/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+// import { deleteExpenseRequest } from "../../../../redux/expense";
+import "./styles.scss";
+
+const VoucherDeleteModal = () => {
+
+  const dispatch = useDispatch();
+
+  const handleVoucherDel = (id) => {
+    // dispatch(deleteVoucherRequest({deleteid: id }));
+  }
+  
+  return (
+    <>
+      {/* <Modal
+        show={showModal}
+        onHide={handleCloseModal}
+        animation={false}
+        centered
+      >
+        <div className="new_modal_data">
+          <div className="warning_icon">
+            <i className="bi bi-trash3-fill delete_it" />
+            <h2>WARNING.!</h2>
+          </div>
+          <div className="data">
+            <p>
+              Deleting the expense will permanently remove it from your list.
+            </p>
+          </div>
+          <div className="btns_two">
+            <Button onClick={() => setShowModal(false)}>No</Button>
+            <Button  onClick={() => handleExpenseDel(id)}>Yes</Button>
+          </div>
+        </div>
+      </Modal> */}
+    </>
+  );
+};
+
+export default VoucherDeleteModal;
